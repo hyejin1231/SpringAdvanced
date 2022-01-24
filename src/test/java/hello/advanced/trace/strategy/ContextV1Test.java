7124ac032966295adb149c706823945334007e46
hyejin1231 <phj981231@naver.com>
2022-01-24 10:19:01 +0900

    20220124 Spring Advanced 전략 패턴 #2 전략 패턴 예제 -2

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -48,6 +49,77 @@ public class ContextV1Test {
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
+    }
+
+    /**
+     * 익명 내부 클래스 사용한 전락 패턴
+     */
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogicV1={}", strategyLogic1.getClass());
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        };
+
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogicV1={}", strategyLogic2.getClass());
+        contextV2.execute();
+    }
+
+
+    /**
+     * 익명 내부 클래스 좀 더 깔끔하게
+     */
+    @Test
+    void strategyV3() {
+
+        ContextV1 contextV1 = new ContextV1( new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+
+    /**
+     * 익명 내부 클래스 람다 사용
+     */
+    @Test
+    void strategyV4() {
+
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비지니스 로직1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비지니스 로직2 실행"));
+        contextV2.execute();
     }
 
 
